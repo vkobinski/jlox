5831adf419b6d88760492862e2ae5f1cf52d460f
vkobinski <victorkobinski@outlook.com>
2024-04-06 20:39:04 -0300

    Parser

--- src/lox/Lox.java
+++ src/lox/Lox.java
@@ -12,29 +12,20 @@ public class Lox {
 
     static boolean hadError = false;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         if(args.length > 1) {
             System.out.println("Usage: jlox [script]");
         } else if (args.length  == 1)  {
             runFile(args[0]);
         } else {
-            try {
-                runPrompt();
-            } catch (Exception e) {
-                System.out.println("Could not read STDIN");
-            }
+            runPrompt();
         }
     }
 
-    private static void runFile(String path) {
-        try {
-            byte[] bytes = Files.readAllBytes(Paths.get(path));
-            run(new String(bytes, Charset.defaultCharset()));
-
-        } catch (IOException ioException) {
-            System.out.println("Could not open [" + path + "]");
-        }
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
 
         if(hadError) System.exit(65);
     }
@@ -71,6 +62,14 @@ public class Lox {
     private static void report(int line, String where, String message) {
         System.out.println("[line " + line + "] Error" + where + ": " + message);
         hadError = true;
+    }
+
+    static void error(Token token, String message) {
+        if(token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
     }
 
 }
--- src/tool/GenerateAst.java
+++ src/tool/GenerateAst.java
@@ -32,14 +32,32 @@ public class GenerateAst {
         writer.println();
         writer.println("abstract class " + baseName + " {");
 
+        defineVisitor(writer, baseName, types);
+
         for(String type: types) {
             String className = type.split(":")[0].trim();
             String fields = type.split(":")[1].trim();
             defineType(writer, baseName, className, fields);
         }
 
+        writer.println();
+        writer.println("    abstract <R> R accept(Visitor<R> visitor);");
+
         writer.println("}");
         writer.close();
+    }
+
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types){
+
+        writer.println(" interface Visitor<R> {");
+
+        for(String type: types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("    R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
+        }
+
+        writer.println("    }");
+
     }
 
     private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
@@ -53,6 +71,12 @@ public class GenerateAst {
             writer.println("    this." + name + " = " + name + ";");
         }
 
+        writer.println("    }");
+
+        writer.println();
+        writer.println("    @Override");
+        writer.println("    <R> R accept(Visitor<R> visitor) {");
+        writer.println("        return visitor.visit" + className + baseName + "(this);");
         writer.println("    }");
 
         writer.println();
