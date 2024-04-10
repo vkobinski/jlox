8236378323e57dbf5fef273bac1f1b47f4e166ae
vkobinski <victorkobinski@outlook.com>
2024-04-09 23:43:44 -0300

    String concatenating conversion

--- src/lox/Interpreter.java
+++ src/lox/Interpreter.java
@@ -44,6 +44,10 @@ public class Interpreter implements Expr.Visitor<Object> {
                 if(left instanceof String && right instanceof String) {
                     return (String) left + (String) right;
                 }
+                if(left instanceof String || right instanceof String) {
+                    return stringify(left) + stringify(right);
+                }
+
                 throw new RuntimeError(expr.operator, "Operands must be two numbers or two strings.");
             case SLASH:
                 checkNumberOperands(expr.operator, left, right);
