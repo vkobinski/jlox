a31533d4637a1cf46d534c2ca734b5c192585b31
vkobinski <victorkobinski@outlook.com>
2024-04-09 22:52:40 -0300

    Binary Expression Interpreter

--- src/lox/Interpreter.java
+++ src/lox/Interpreter.java
@@ -3,6 +3,40 @@ package lox;
 public class Interpreter implements Expr.Visitor<Object> {
     @Override
     public Object visitBinaryExpr(Expr.Binary expr) {
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case GREATER:
+                return (double) left > (double) right;
+            case GREATER_EQUAL:
+                return (double) left >= (double) right;
+            case LESS:
+                return (double) left < (double) right;
+            case LESS_EQUAL:
+                return (double) left <= (double) right;
+            case MINUS:
+                return (double) left - (double) right;
+            case BANG_EQUAL:
+                return !isEqual(left, right);
+            case EQUAL_EQUAL:
+                return isEqual(left, right);
+            case PLUS:
+                if(left instanceof Double && right instanceof Double) {
+                    return (double) left + (double) right;
+                }
+
+                if(left instanceof String && right instanceof String) {
+                    return (String) left + (String) right;
+                }
+                break;
+            case SLASH:
+                return (double) left / (double) right;
+            case STAR:
+                return (double) left * (double) right;
+
+        }
+
         return null;
     }
 
@@ -32,5 +66,20 @@ public class Interpreter implements Expr.Visitor<Object> {
 
     private Object evaluate(Expr expr) {
         return expr.accept(this);
+    }
+
+    private boolean isTruthy(Object object) {
+        if(object == null) return false;
+        if(object instanceof Boolean) return (boolean) object;
+
+        return true;
+    }
+
+    private boolean isEqual(Object a, Object b) {
+
+        if(a == null && b == null) return true;
+        if(a == null) return false;
+
+        return a.equals(b);
     }
 }
